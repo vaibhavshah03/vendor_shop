8e847ae8f32e9ee6c7fe6b0e4c7df30acaa06878
Vaibhav shah <vaibhavshah6403@gmail.com>
2024-02-13 13:15:20 +0530

    Admin Panel Done

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,16 @@ import Error from "./components/Error/Error";
 import Login from "./components/Login/Login";
 import Admin from "./components/Login/Admin";
 import Register from "./components/Login/Register";
+import LabourRegister from "./components/LabourPanel/LabourRegister";
+import LabourLogin from "./components/LabourPanel/LabourLogin";
+import LabourPanel from "./components/LabourPanel/LabourPanel";
+import LabourOrders from "./components/LabourPanel/LabourOrders";
+import EditLabourProfile from "./components/LabourPanel/EditLabourProfile";
+import AdminPanel from "./components/AdminPanel/AdminPanel";
+import Manageuser from "./components/AdminPanel/ManageUser";
+import AddProducts from "./components/AdminPanel/AddProducts";
+import ManageOrder from "./components/AdminPanel/ManageOrder";
+import ManageLabour from "./components/AdminPanel/ManageLabour";
 
 import AppContext from "./utils/Context";
 
@@ -23,6 +33,17 @@ function App() {
                         <Route path='/login' element={<Login/>}/>
                         <Route path='/register' element={<Register/>}/>
                         <Route path='/admin' element={<Admin/>}/>
+                        <Route path='/labour/register' element={<LabourRegister/>}/>
+                        <Route path='/labour/login' element={<LabourLogin/>}/>
+                        <Route path='/labour/panel' element={<LabourPanel/>}/>
+                        <Route path='/labour/orders' element={<LabourOrders/>}/>
+                        <Route path='/labour/edit' element={<EditLabourProfile/>}/>
+                        <Route path='/admin' element={<Admin/>}/>
+                        <Route path='/adminpanel' element={<AdminPanel/>}/>
+                        <Route path='/adminpanel/users' element={<Manageuser/>}/>
+                        <Route path='/adminpanel/addproducts' element={<AddProducts/>}/>
+                        <Route path='/adminpanel/manageorders' element={<ManageOrder/>}/>
+                        <Route path='/adminpanel/managelabours' element={<ManageLabour/>}/>
                         <Route path='*' element={<Error/>}/>
                     </Routes>
                     <Footer/>
