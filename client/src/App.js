ddc73e7c74d3d4887cbb53f41146f3e7038004d0
Vaibhav shah <vaibhavshah6403@gmail.com>
2024-02-13 13:25:25 +0530

    client side done

--- client/src/App.js
+++ client/src/App.js
@@ -3,29 +3,30 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Error from "./components/Error/Error";
+import SingleProduct from "./components/SingleProduct/SingleProduct";
 import Login from "./components/Login/Login";
 import Admin from "./components/Login/Admin";
 import Register from "./components/Login/Register";
-import LabourRegister from "./components/LabourPanel/LabourRegister";
-import LabourLogin from "./components/LabourPanel/LabourLogin";
-import LabourPanel from "./components/LabourPanel/LabourPanel";
-import LabourOrders from "./components/LabourPanel/LabourOrders";
-import EditLabourProfile from "./components/LabourPanel/EditLabourProfile";
 import AdminPanel from "./components/AdminPanel/AdminPanel";
+import AppContext from "./utils/Context";
+import Cart from "./components/SingleProduct/Cart/Cart";
+import AllProducts from "./components/Product/AllProducts";
 import Manageuser from "./components/AdminPanel/ManageUser";
 import AddProducts from "./components/AdminPanel/AddProducts";
 import ManageOrder from "./components/AdminPanel/ManageOrder";
-import ManageLabour from "./components/AdminPanel/ManageLabour";
 import UserDashboard from "./components/UserDashboard/UserDashboard";
 import ViewOrders from "./components/UserDashboard/ViewOrders";
 import ProfileDetails from "./components/UserDashboard/ProfileDetails";
+import About from "./components/Home/About";
 import Notification from "./components/UserDashboard/Notification";
-import EditProfile from "./components/UserDashboard/EditProfile";
-
-import AppContext from "./utils/Context";
-
-
+import LabourRegister from "./components/LabourPanel/LabourRegister";
+import LabourLogin from "./components/LabourPanel/LabourLogin";
+import ManageLabour from "./components/AdminPanel/ManageLabour";
+import LabourPanel from "./components/LabourPanel/LabourPanel";
+import LabourOrders from "./components/LabourPanel/LabourOrders";
 import Footer from "./components/Footer/Footer";
+import EditProfile from "./components/UserDashboard/EditProfile";
+import EditLabourProfile from "./components/LabourPanel/EditLabourProfile";
 
 function App() {
     return (
@@ -54,6 +55,10 @@ function App() {
                         <Route path='/userdashboard/profile' element={<ProfileDetails/>}/>
                         <Route path='/userdashboard/edit' element={<EditProfile/>}/>
                         <Route path='/userdashboard/notifications' element={<Notification/>}/>
+                        <Route path='/product/:slug' element={<SingleProduct/>}/>
+                        <Route path='/cart' element={<Cart/>}/>
+                        <Route path='/products' element={<AllProducts/>}/>
+                        <Route path='/about' element={<About/>}/>
                         <Route path='*' element={<Error/>}/>
                     </Routes>
                     <Footer/>
